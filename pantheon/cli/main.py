2ae24f5f5055b9f192776fdec3a78bd16f480cfe
Starlitnightly <starlitnightly@163.com>
2025-08-11 16:00:30 -0700

    Added endpoint in terminal. You can run `pantheon-cli` directly launch cli

---
+++ pantheon/cli/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Pantheon CLI - Command line entry point for pip installation"""
+
+from .core import cli
+
+def main():
+    """Main entry point for pantheon-cli command"""
+    cli()
+
+if __name__ == "__main__":
+    main()
