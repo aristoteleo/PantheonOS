3f536d5b065df119d08a002f1dadda750c25ffc1
Rwin2 <erwin.poussi@polytechnique.edu>
2025-11-25 22:52:38 -0800

    timeout

--- examples/gene_panel_selection/panel_selection_team.py
+++ examples/gene_panel_selection/panel_selection_team.py
@@ -18,7 +18,7 @@ from pantheon.team.aat import AgentAsToolTeam
 from pantheon.utils.display import print_agent_message
 
 
-TIMEOUT_TOOL = 1*60*60
+TIMEOUT_TOOL = 24*60*60
 
 
 async def load_agent(path: str):
